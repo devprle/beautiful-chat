bff7e25aedcaa3af984ba0360f30261340e60ac4
Aleksandar <badprle@gmail.com>
2023-12-18 16:02:29 +0100

    Made app more prettier

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,13 +1,15 @@
-import {MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced'
+import {PrettyChatWindow} from 'react-chat-engine-pretty'
 
 const ChatsPage = (props) => {
 
-    const chatProps = useMultiChatLogic('ee3b9fee-f35b-4534-b1e9-74152ecdb3c6', props.user.username, props.user.secret)
 
     return <div style={{height: '100vh'}}>
-        <MultiChatSocket {...chatProps}/>
-        <MultiChatWindow {...chatProps} style={{height: '100vh'}}/>
-
+        <PrettyChatWindow
+            projectId='ee3b9fee-f35b-4534-b1e9-74152ecdb3c6'
+            username={props.user.username}
+            secret={props.user.secret}
+            style={{height: '100dvh'}}
+        />
     </div>
 }
 
