3b3d73f4df2d0df5402ae6791ce5cb45678551a2
Aleksandar <badprle@gmail.com>
2023-12-18 15:04:48 +0100

    Creating user added

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: true }));
+app.use(cors({origin: true}));
 
 app.post("/authenticate", async (req, res) => {
-    const { username } = req.body;
-    return res.json({ username: username, secret: "sha256..." });
+    const {username} = req.body;
+
+    try {
+        const r = await axios.put('https://api.chatengine.io/users', {
+            username: username, secret: username, first_name: username
+        }, {
+            headers: {"private-key": '4e796bb6-11c4-4aab-9039-624f09010344'}
+        })
+
+        return res.status(r.status).json(r.data)
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data)
+    }
 });
 
 app.listen(3001);
