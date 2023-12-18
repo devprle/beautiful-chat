dc1cf3f914fe579b671147b379047023bd252f4f
Aleksandar <badprle@gmail.com>
2023-12-18 15:39:24 +0100

    Adding react chat engine components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,3 +14,6 @@ function App() {
         return <ChatsPage user={user} />;
     }
 }
+
+
+export default App
--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,14 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import {MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+
+    const chatProps = useMultiChatLogic('ee3b9fee-f35b-4534-b1e9-74152ecdb3c6', props.user.username, props.user.secret)
+
+    return <div style={{height: '100vh'}}>
+        <MultiChatSocket {...chatProps}/>
+        <MultiChatWindow {...chatProps} style={{height: '100vh'}}/>
+
+    </div>
 }
 
 export default ChatsPage
